7167a2cdc51ebb190a241f18632a08ecb6c60234
Pedro Fernandes <pedropbfernandes@gmail.com>
2020-07-14 23:53:11 -0300

    Source.Test Add

---
+++ Source.TestSubmission/CompanyModelTest.cs
@@ -0,0 +1,23 @@
+﻿using Codenation.Challenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Codenation.Challenge
+{
+    public sealed class CompanyModelTest : ModelBaseTest
+    {
+        public CompanyModelTest() : base(new CodenationContext())
+        {
+            Model = "Codenation.Challenge.Models.Company";
+            Table = "company";
+        }
+
+        [Fact]
+        public void Should_Has_Table()
+        {
+            AssertTable();
+        }
+    }
+}
